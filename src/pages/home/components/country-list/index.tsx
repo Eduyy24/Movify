ca973e31eecca756dae1baa3d0139050600eeecb
Eduardo Pinedo <eduyy_24@hotmail.com>
2023-05-11 02:51:51 -0500

    fisrt implement flatlist for countries

--- src/pages/home/components/country-list/index.tsx
+++ src/pages/home/components/country-list/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import {useGeneralContext} from '../../../../hooks/useGeneralContext';
+import {FlatList, View, Text} from 'react-native';
 
 export const CountryList = () => {
-  return <></>;
+  const {countryList} = useGeneralContext();
+  return (
+    <View>
+      <FlatList
+        data={countryList}
+        renderItem={({item}) => <Text>{item}</Text>}
+      />
+    </View>
+  );
 };
